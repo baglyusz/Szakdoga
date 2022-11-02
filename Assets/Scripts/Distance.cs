3003e792bfe5699d27d491e1c08e8f65c7b6ba5f
baglyusz <65501182+baglyusz@users.noreply.github.com>
2022-11-02 19:30:23 +0100

    puzzle working

---
+++ Assets/Scripts/Distance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Serialization;
+
+public class Distance : MonoBehaviour {
+	public GameObject key;
+    public GameObject unlock;
+    public GameObject book;
+    private bool unlocked = false;
+
+    private void Start () 
+    {
+        book.SetActive(false);
+        key.SetActive(true);
+        unlock.SetActive(true);
+    }
+
+    private void Update () {
+        var distance = Math.Abs(Vector3.Distance(key.transform.position, unlock.transform.position));
+        if (distance < 0.05 && !unlocked)
+        {
+            unlocked = true;
+            book.SetActive(true);
+            key.SetActive(false);
+            unlock.SetActive(false);
+        }
+        //else
+        //{
+        //    book.SetActive(false);
+        //    key.SetActive(true);
+        //    unlock.SetActive(true);
+        //}
+    }
+}
