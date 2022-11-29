8f66fabd7e34fdbb95fb1ebe40fbd962ca8b4364
baglyusz <65501182+baglyusz@users.noreply.github.com>
2022-11-29 11:29:03 +0100

    Connect to server, create or/and join room

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,99 +1,99 @@
-using Photon.Pun;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+//using Photon.Pun;
+//using System.Collections.Generic;
+//using System.Linq;
+//using UnityEngine;
 
-public class GameManager : MonoBehaviourPunCallbacks
-{
-    [Header("Status")]
-    public bool gameEnded = false;
+//public class GameManager : MonoBehaviourPunCallbacks
+//{
+//    [Header("Status")]
+//    public bool gameEnded = false;
 
-    [Header("Players")]
-    public string playerPrefabLocation;
+//    [Header("Players")]
+//    public string playerPrefabLocation;
 
-    public Transform[] spawnPoints;
+//    public Transform[] spawnPoints;
 
-    public PlayerController[] players;
+//    public PlayerController[] players;
 
-    private int playersInGame;
+//    private int playersInGame;
 
-    private List<int> pickedSpawnIndex;
+//    private List<int> pickedSpawnIndex;
 
-    [Header("Reference")]
-    public GameObject imageTarget;
+//    [Header("Reference")]
+//    public GameObject imageTarget;
     
-    //instance
-    public static GameManager Instance;
+//    //instance
+//    public static GameManager Instance;
 
-    private void Awake()
-    {
-        Instance = this;
-    }
+//    private void Awake()
+//    {
+//        Instance = this;
+//    }
 
-    public PhotonView view;
+//    public PhotonView view;
 
-    private void Start()
-    {
-        pickedSpawnIndex = new List<int>();
-        players = new PlayerController[PhotonNetwork.PlayerList.Length];
-        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
-       // DefaultObserverEventHandler.isTracking = false;
-    }
+//    private void Start()
+//    {
+//        pickedSpawnIndex = new List<int>();
+//        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+//        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+//       // DefaultObserverEventHandler.isTracking = false;
+//    }
 
-    private void Update()
-    {
-        //Debug.Log("is tracking" + DefaultObserverEventHandler.isTracking);
+//    private void Update()
+//    {
+//        //Debug.Log("is tracking" + DefaultObserverEventHandler.isTracking);
 
-        foreach (var gameObj in FindObjectsOfType(typeof(GameObject)).Cast<GameObject>())
-        {
-            if (gameObj.name == "Player(Clone)")
-            {
-                gameObj.transform.SetParent(imageTarget.transform);
-            }
-        }
+//        foreach (var gameObj in FindObjectsOfType(typeof(GameObject)).Cast<GameObject>())
+//        {
+//            if (gameObj.name == "Player(Clone)")
+//            {
+//                gameObj.transform.SetParent(imageTarget.transform);
+//            }
+//        }
 
-        for (int i = 1; i < imageTarget.transform.childCount; i++)
-        {
+//        for (int i = 1; i < imageTarget.transform.childCount; i++)
+//        {
           
-            //imageTarget.transform.GetChild(i).gameObject.SetActive(DefaultObserverEventHandler.isTracking);
-        }
-    }
+//            //imageTarget.transform.GetChild(i).gameObject.SetActive(DefaultObserverEventHandler.isTracking);
+//        }
+//    }
 
-    [PunRPC]
-    void ImInGame()
-    {
-        playersInGame++;
-        if (playersInGame == PhotonNetwork.PlayerList.Length)
-        {
-            SpawnPlayer();
-        }
-    }
+//    [PunRPC]
+//    void ImInGame()
+//    {
+//        playersInGame++;
+//        if (playersInGame == PhotonNetwork.PlayerList.Length)
+//        {
+//            SpawnPlayer();
+//        }
+//    }
 
-    void SpawnPlayer(){
+//    void SpawnPlayer(){
        
-        var rand = Random.Range(0, spawnPoints.Length);
+//        var rand = Random.Range(0, spawnPoints.Length);
 
-        while (pickedSpawnIndex.Contains(rand))
-        {
-            rand = Random.Range(0, spawnPoints.Length);
-        }
+//        while (pickedSpawnIndex.Contains(rand))
+//        {
+//            rand = Random.Range(0, spawnPoints.Length);
+//        }
 
-        pickedSpawnIndex.Add(rand);
+//        pickedSpawnIndex.Add(rand);
         
-        GameObject playerObject = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[rand].position, Quaternion.identity);
+//        GameObject playerObject = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[rand].position, Quaternion.identity);
                                                                                                                                                               
-        PlayerController playerScript = playerObject.GetComponent<PlayerController>();
+//        PlayerController playerScript = playerObject.GetComponent<PlayerController>();
 
-        playerScript.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
-    }
+//        playerScript.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+//    }
 
-    public PlayerController GetPlayer(int playerID)
-    {
-        return players.First(x => x.id == playerID);
-    }
+//    public PlayerController GetPlayer(int playerID)
+//    {
+//        return players.First(x => x.id == playerID);
+//    }
 
-    public PlayerController GetPlayer(GameObject playerObj)
-    {
-        return players.First(x => x.gameObject == playerObj);
-    }
-}
+//    public PlayerController GetPlayer(GameObject playerObj)
+//    {
+//        return players.First(x => x.gameObject == playerObj);
+//    }
+//}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,107 +1,107 @@
-using Photon.Pun;
-using Photon.Realtime;
-using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
+//using Photon.Pun;
+//using Photon.Realtime;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class MenuManager : MonoBehaviourPunCallbacks
-{
+//public class MenuManager : MonoBehaviourPunCallbacks
+//{
 
-    [Header(" — -Menus — -")]
-    public GameObject mainMenu;
-    public GameObject lobbyMenu;
+//    [Header(" — -Menus — -")]
+//    public GameObject mainMenu;
+//    public GameObject lobbyMenu;
 
-    [Header(" — -Main Menu — -")]
-    public Button createRoomBtn;
+//    [Header(" — -Main Menu — -")]
+//    public Button createRoomBtn;
 
-    public Button joinRoomBtn;
-    [Header(" — -Lobby Menu — -")]
-    public TMP_InputField roomName;
+//    public Button joinRoomBtn;
+//    [Header(" — -Lobby Menu — -")]
+//    public TMP_InputField roomName;
 
-    public TMP_InputField playerList;
+//    public TMP_InputField playerList;
 
-    public Button startGameBtn;
+//    public Button startGameBtn;
 
-    private void Start()
-    {
-        createRoomBtn.interactable = false;
-        joinRoomBtn.interactable = false;
-    }
+//    private void Start()
+//    {
+//        createRoomBtn.interactable = false;
+//        joinRoomBtn.interactable = false;
+//    }
 
-    public override void OnConnectedToMaster()
-    {
-        createRoomBtn.interactable = true;
-        joinRoomBtn.interactable = true;
-    }
+//    public override void OnConnectedToMaster()
+//    {
+//        createRoomBtn.interactable = true;
+//        joinRoomBtn.interactable = true;
+//    }
 
-    private void SetMenu(GameObject menu)
-    {
-        mainMenu.SetActive(false);
-        lobbyMenu.SetActive(false);
-        menu.SetActive(true);
-    }
+//    private void SetMenu(GameObject menu)
+//    {
+//        mainMenu.SetActive(false);
+//        lobbyMenu.SetActive(false);
+//        menu.SetActive(true);
+//    }
 
-    public void OnCreateRoomBtn(TextMeshProUGUI roomNameInput)
-    {
-        string BogiSaidString = roomName.text;
-        NetworkManager.instance.CreateRoom(BogiSaidString);
-        Debug.Log(BogiSaidString);
-        roomName.text = BogiSaidString;
-        /*    UIText = GameObject.FindGameObjectWithTag("Text").GetComponent<Text>();
-           UIText.text = gameObject.GetComponent<Text>().text;
-           */
-    }
+//    public void OnCreateRoomBtn(TextMeshProUGUI roomNameInput)
+//    {
+//        string BogiSaidString = roomName.text;
+//        NetworkManager.instance.CreateRoom(BogiSaidString);
+//        Debug.Log(BogiSaidString);
+//        roomName.text = BogiSaidString;
+//        /*    UIText = GameObject.FindGameObjectWithTag("Text").GetComponent<Text>();
+//           UIText.text = gameObject.GetComponent<Text>().text;
+//           */
+//    }
 
-    public void OnJoinRoomBtn(TextMeshProUGUI roomNameInput)
-    {
-        NetworkManager.instance.JoinRoom(roomNameInput.text);
-        roomName.text = roomNameInput.text;
-    }
+//    public void OnJoinRoomBtn(TextMeshProUGUI roomNameInput)
+//    {
+//        NetworkManager.instance.JoinRoom(roomNameInput.text);
+//        roomName.text = roomNameInput.text;
+//    }
 
-    public void OnPlayerNameUpdate(TextMeshProUGUI playerNameInput)
-    {
-        PhotonNetwork.NickName = playerNameInput.text;
-    }
+//    public void OnPlayerNameUpdate(TextMeshProUGUI playerNameInput)
+//    {
+//        PhotonNetwork.NickName = playerNameInput.text;
+//    }
 
-    public override void OnJoinedRoom()
-    {
-        SetMenu(lobbyMenu);
-        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
-    }
+//    public override void OnJoinedRoom()
+//    {
+//        SetMenu(lobbyMenu);
+//        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+//    }
 
-    public override void OnPlayerLeftRoom(Player otherPlayer)
-    {
-        UpdateLobbyUI();
-    }
+//    public override void OnPlayerLeftRoom(Player otherPlayer)
+//    {
+//        UpdateLobbyUI();
+//    }
 
-    [PunRPC]
-    public void UpdateLobbyUI()
-    {
-        playerList.text = "";
+//    [PunRPC]
+//    public void UpdateLobbyUI()
+//    {
+//        playerList.text = "";
 
-        foreach (Player player in PhotonNetwork.PlayerList)
-        {
-            if (player.IsMasterClient)
-            {
-                playerList.text += player.NickName + " (Host) \n";
-            }
-            else
-            {
-                playerList.text += player.NickName + "\n";
-            }
-        }
+//        foreach (Player player in PhotonNetwork.PlayerList)
+//        {
+//            if (player.IsMasterClient)
+//            {
+//                playerList.text += player.NickName + " (Host) \n";
+//            }
+//            else
+//            {
+//                playerList.text += player.NickName + "\n";
+//            }
+//        }
 
-        startGameBtn.interactable = PhotonNetwork.IsMasterClient;
-    }
+//        startGameBtn.interactable = PhotonNetwork.IsMasterClient;
+//    }
 
-    public void OnLeaveLobbyBtn()
-    {
-        PhotonNetwork.LeaveRoom(); 
-        SetMenu(mainMenu);
-    }
+//    public void OnLeaveLobbyBtn()
+//    {
+//        PhotonNetwork.LeaveRoom(); 
+//        SetMenu(mainMenu);
+//    }
 
-    public void OnStartGameBtn()
-    {
-        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "PlantsVsPlants");
-    }
-}
+//    public void OnStartGameBtn()
+//    {
+//        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "PlantsVsPlants");
+//    }
+//}
