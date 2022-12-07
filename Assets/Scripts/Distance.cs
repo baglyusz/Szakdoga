ffd6aa3f0f3705ca35a3cf1ab656b95d1981ec15
baglyusz <65501182+baglyusz@users.noreply.github.com>
2022-12-07 23:21:22 +0100

    fixed stuff

--- Assets/Scripts/Distance.cs
+++ Assets/Scripts/Distance.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class Distance : MonoBehaviour
 {
+    [SerializeField]
+    private Canvas canvas;
+
     public GameObject key;
     public GameObject unlock;
     public GameObject book;
@@ -10,6 +13,7 @@ public class Distance : MonoBehaviour
 
     private void Start()
     {
+        canvas.gameObject.SetActive(false);
         book.SetActive(false);
         key.SetActive(true);
         unlock.SetActive(true);
@@ -18,9 +22,12 @@ public class Distance : MonoBehaviour
     private void Update()
     {
         var distance = Math.Abs(Vector3.Distance(key.transform.position, unlock.transform.position));
+
         if (!(distance < 0.05) || _unlocked) return;
         _unlocked = true;
+
         book.SetActive(true);
+        canvas.gameObject.SetActive(true);
         key.SetActive(false);
         unlock.SetActive(false);
     }
--- Assets/Scripts/GetBook.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GetBook : MonoBehaviour, IInteractable
-{
-    public void Interact()
-    {
-        throw new System.NotImplementedException();
-    }
-}
